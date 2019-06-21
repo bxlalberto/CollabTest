b62605cf4662ad060498c79452f824687b0ce30d
Sam Edison <sdedison189@gmail.com>
2019-06-21 19:07:54 -0400

    initial commit

---
+++ src/LoopFun.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+/**
+ * Visualizes a random input stream of integers between 1 and 10.
+ * @author sdedison
+ *
+ */
+
+public class LoopFun {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		for (int i = 0; i < 50; i++) {
+			int theNum = rand.nextInt(10) + 1;
+			if (theNum == 1) {
+				System.out.println("*---------");
+			} else if (theNum == 2) {
+				System.out.println("**--------");	
+			} else if (theNum == 3) {
+				System.out.println("***-------");
+			} else if (theNum == 4) {
+				System.out.println("****------");
+			} else if (theNum == 5) {
+				System.out.println("*****-----");
+			} else if (theNum == 6) {
+				System.out.println("******----");
+			} else if (theNum == 7) {
+				System.out.println("*******---");
+			} else if (theNum == 8) {
+				System.out.println("********--");
+			} else if (theNum == 9) {
+				System.out.println("*********-");
+			} else if (theNum == 10) {
+				System.out.println("**********");
+			}
+		}
+	}
+}
