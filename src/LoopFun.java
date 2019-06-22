1b921e45a0ed07984c666521444c86fa2745b7e0
Sam Edison <sdedison189@gmail.com>
2019-06-21 20:10:17 -0400

    changed loop iterations to 100 vs. 50

--- src/LoopFun.java
+++ src/LoopFun.java
@@ -10,7 +10,7 @@ public class LoopFun {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < 100; i++) {
 			int theNum = rand.nextInt(10) + 1;
 			if (theNum == 1) {
 				System.out.println("*---------");
